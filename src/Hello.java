93f60bea1e3d8808d7495a614fbe67f362f70f69
Arpine Kasaryan <arpinekasaryan@Arpines-MacBook-Pro.local>
2021-03-14 14:02:32 +0400

    Added method which will convert string form lowercase to uppercase

--- src/Hello.java
+++ src/Hello.java
@@ -1,33 +1,28 @@
-// Java program to Convert characters
-// of a string to opposite case
-class Test {
+public class Hello {
+    static String LowerToUpper(String s) {
+        String result = "";
+        char ch = ' ';
+        for (int i = 0; i < s.length(); i++) {
 
-    // Method to convert characters
-    // of a string to opposite case
-    static void convertOpposite(StringBuffer str)
-    {
-        int ln = str.length();
+            //check valid alphabet and it is in lowercase
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                ch = (char)(s.charAt(i) - 32);
+            }
+            //else keep the same alphabet or any character
+            else {
+                ch = (char)(s.charAt(i));
+            }
 
-        // Conversion using predefined methods
-        for (int i = 0; i < ln; i++) {
-            Character c = str.charAt(i);
-            if (Character.isLowerCase(c))
-                str.replace(i, i + 1,
-                        Character.toUpperCase(c) + "");
-            else
-                str.replace(i, i + 1,
-                        Character.toLowerCase(c) + "");
+            result += ch; // concatenation, append c to result
         }
+        return result;
     }
 
-    public static void main(String[] args)
-    {
-        StringBuffer str
-                = new StringBuffer("aaaaa");
-        // Calling the Method
-        convertOpposite(str);
-
-        System.out.println(str);
+    public static void main(String[] args) {
+        System.out.println(LowerToUpper("jjj"));
+        System.out.println(LowerToUpper("www.example.com"));
+        System.out.println(LowerToUpper("123abcd@9081"));
+        System.out.println(LowerToUpper("OKAY@123"));
     }
 }
 
