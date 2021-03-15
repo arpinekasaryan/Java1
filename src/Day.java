1e1c73702748c1f6e12e8db66b44937bfcadf7a2
Arpine Kasaryan <arpinekasaryan@Arpines-MacBook-Pro.local>
2021-03-15 18:39:07 +0400

    Print Name

---
+++ src/Day.java
@@ -0,0 +1,11 @@
+public class Day {
+    public static void main(String[] args) {
+        String a = "great";
+
+
+        String as = a.toUpperCase();
+
+        System.out.println(as);
+        }
+    }
+
